8aa12cc5bb2637cf18ab7094c9b1e2e74538c2b1
petem46 <petem46@googlemail.com>
2020-04-16 10:55:41 +0100

    quick commit

--- routes/web.php
+++ routes/web.php
@@ -13,63 +13,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
-|- COURSE ROUTES
-*/
-Route::get('/get/courselist', 'CategoryController@index');
-Route::get('/get/c', 'CategoryController@index');
-Route::get('/get/c/details/{name}', 'CourseController@details');
-
-/*
-|- CATEGORY ROUTES
-*/
-Route::get('/get/cc/catfilter', 'CategoryController@catfilter');
-Route::get('/get/cc', 'CategoryController@list');
-Route::get('/get/cats', 'CategoryController@list');
-Route::get('/get/cat/list', 'CategoryController@list');
-Route::get('/get/cc/list', 'CategoryController@list');
-Route::get('/get/cat/{catname}', 'CategoryController@show');
-
-/*
-|- USER ROUTES
-*/
-Route::get('/get/u/getMyCourses', 'CourseController@getMyCourses');
-Route::put('/put/u/addToMyCourses/{course_id}/{state_id}',  'CourseController@addToMyCourses');
-Route::put('/put/u/addRating/{course_id}/{rating}',  'CourseController@addRating');
-Route::delete('/delete/u/deleteFromMyCourses/{course_id}/',  'CourseController@deleteFromMyCourses');
-Route::get('/get/u/getdashboarddata', 'CourseController@dashboarddata');
-
-/*
-|- MANAGE USERS ROUTES
-*/
-Route::get('/get/u/all', 'UsersController@overview');
-
-/*
-|- MANAGE COURSES ROUTES
-*/
-Route::get('/get/c/all', 'CourseController@overview');
-Route::post('/post/c/savecourse', 'CourseController@savecourse');
-
-/*
-|- MANAGE REVIEWS ROUTES
-*/
-Route::get('/get/r/all', 'ReviewController@overview');
-Route::get('/get/r/reviewers', 'ReviewController@reviewers');
-Route::post('/post/r/savereview', 'ReviewController@savereview');
-
-
-// Route::resource('cc', 'CategoryController');
-// Route::resource('c', 'CourseController');
-
 Auth::routes();
-
 
 Route::get('/redirect', 'SocialAuthGoogleController@redirect');
 Route::get('/callback', 'SocialAuthGoogleController@callback');
 
+
 Route::group(['middleware' => ['auth']], function () {
 
+  /*
+|- COURSE ROUTES
+*/
+  Route::get('/get/courselist', 'CategoryController@index');
+  Route::get('/get/c', 'CategoryController@index');
+  Route::get('/get/c/details/{name}', 'CourseController@details');
 
+  /*
+|- CATEGORY ROUTES
+*/
+  Route::get('/get/cc/catfilter', 'CategoryController@catfilter');
+  Route::get('/get/cc', 'CategoryController@list');
+  Route::get('/get/cats', 'CategoryController@list');
+  Route::get('/get/cat/list', 'CategoryController@list');
+  Route::get('/get/cc/list', 'CategoryController@list');
+  Route::get('/get/cat/{catname}', 'CategoryController@show');
+
+  /*
+|- USER ROUTES
+*/
+  Route::get('/get/u/getMyCourses', 'CourseController@getMyCourses');
+  Route::put('/put/u/addToMyCourses/{course_id}/{state_id}',  'CourseController@addToMyCourses');
+  Route::put('/put/u/addRating/{course_id}/{rating}',  'CourseController@addRating');
+  Route::delete('/delete/u/deleteFromMyCourses/{course_id}/',  'CourseController@deleteFromMyCourses');
+  Route::get('/get/u/getdashboarddata', 'CourseController@dashboarddata');
+
+  /*
+|- MANAGE USERS ROUTES
+*/
+  Route::get('/get/u/all', 'UsersController@overview');
+
+  /*
+|- MANAGE COURSES ROUTES
+*/
+  Route::get('/get/c/all', 'CourseController@overview');
+  Route::post('/post/c/savecourse', 'CourseController@savecourse');
+
+  /*
+|- MANAGE REVIEWS ROUTES
+*/
+  Route::get('/get/r/all', 'ReviewController@overview');
+  Route::get('/get/r/reviewers', 'ReviewController@reviewers');
+  Route::post('/post/r/savereview', 'ReviewController@savereview');
+
+
+  // Route::resource('cc', 'CategoryController');
+  // Route::resource('c', 'CourseController');
 
   Route::get('/{any}', 'SpaController@index')->where('any', '.*');
 });
