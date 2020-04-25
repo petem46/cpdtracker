99c491c8d3623dcb88ea7162c9362a22e0a6c112
petem46 <petem46@googlemail.com>
2020-04-25 20:45:09 +0100

    quick commit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,17 +14,17 @@ class UserSeeder extends Seeder
     public function run(Faker $faker)
     {
       DB::table('users')->insert(array(
-        array(
-          'name' => 'Pete Montgomery',
-          'email' => 'pete.montgomery@fcat.org.uk',
-          'school' => 'FCAT',
-          'email_verified_at' => now(),
-          'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-          'remember_token' => Str::random(10),
-          'avatar' => 'https://www.placecage.com/295/165',
-          'usergroup_id' => 1,
-          'role_id' => 1,
-        ),
+        // array(
+        //   'name' => 'Pete Montgomery',
+        //   'email' => 'pete.montgomery@fcat.org.uk',
+        //   'school' => 'FCAT',
+        //   'email_verified_at' => now(),
+        //   'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //   'remember_token' => Str::random(10),
+        //   'avatar' => 'https://www.placecage.com/295/165',
+        //   'usergroup_id' => 1,
+        //   'role_id' => 1,
+        // ),
         array(
           'name' => 'Demo Admin',
           'email' => 'demoadmin@cpdtracker.co.uk',
@@ -47,30 +47,8 @@ class UserSeeder extends Seeder
           'usergroup_id' => 3,
           'role_id' => 3,
         ),
-        array(
-          'name' => 'Aspire Testing',
-          'email' => 'testing@aspire.fcat.org.uk',
-          'school' => 'Aspire',
-          'email_verified_at' => now(),
-          'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-          'remember_token' => Str::random(10),
-          'avatar' => 'https://www.placecage.com/295/165',
-          'usergroup_id' => 1,
-          'role_id' => 1,
-        ),
-        array(
-          'name' => 'Jane McCumiskey',
-          'email' => 'jane.mccumiskey@fcat.org.uk',
-          'school' => 'FCAT',
-          'email_verified_at' => now(),
-          'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-          'remember_token' => Str::random(10),
-          'avatar' => 'https://www.placecage.com/295/165',
-          'usergroup_id' => 1,
-          'role_id' => 1,
-        ),
       ));
-      $count = 49;
+      $count = 47;
       factory(User::class, $count)->create();
 
     }
