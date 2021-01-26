3e8c5c112f7ce06ad6f31b5ae935bec4c9c5da7f
petem46 <petem46@googlemail.com>
2021-01-26 13:17:43 +0000

    quick commit

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         /*
          * Socialite Google Auth Provider...
          */
-        // Laravel\Socialite\SocialiteServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class
 
     ],
@@ -227,7 +227,7 @@ return [
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        // 'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
