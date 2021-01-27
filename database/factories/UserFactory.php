6a304b9cb91cc00fa8be3dbdcd6a0ef839f20c0a
petem46 <petem46@googlemail.com>
2021-01-27 16:38:27 +0000

    quick commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'avatar' => 'https://www.placecage.com/295/165',
+        'avatar' => 'https://www.placecage.com/295/255',
         'usergroup_id' => $faker->randomDigit($min = 1, $max = 5),
         'role_id' => $faker->randomDigit($min = 1, $max = 3),
     ];
